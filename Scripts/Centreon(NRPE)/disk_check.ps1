dbb6744d6c9127cee06e7e96e1114d5f655fe233
Matthew Jones <127968588+MatthewAJones-Net@users.noreply.github.com>
2026-01-06 17:17:18 +0000

    Add files via upload

---
+++ Scripts/Centreon(NRPE)/disk_check.ps1
@@ -0,0 +1,10 @@
+﻿$disk = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType=3" | Select-Object DeviceID, Size, FreeSpace
+
+foreach ($d in $disk) {
+    $deviceID = $d.DeviceID
+    $size = [math]::Round($d.Size / 1GB, 2)
+    $freeSpace = [math]::Round($d.FreeSpace / 1GB, 2)
+    $usedSpace = [math]::Round(($d.Size - $d.FreeSpace) / 1GB, 2)
+    
+    Write-Host "DISK-$deviceID Size:$size GB Used:$usedSpace GB Free:$freeSpace GB"
+}
